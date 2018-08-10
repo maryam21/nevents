26b1fd2a84a9243955a04f387ab53e9f6aa70ef9
mariam <mariam2ac1@gmail.com>
2018-08-10 15:57:49 +0100

    Fetch events from json file

--- components/EventList.js
+++ components/EventList.js
@@ -3,11 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 export default class EventList extends React.Component {
+    state = {
+        events: []
+    }
+
+    componentDidMount() {
+        const events = require('../db.json').events;
+        this.setState({ events });
+    }
+
     render() {
         return (
         <FlatList 
-            data={[{name: 'first'}, {name: 'second'}]}
-            renderItem={(({item}) => <Text>{item.name}</Text>)}
+            data={this.state.events}
+            renderItem={(({item}) => <Text>{item.title}</Text>)}
+            keyExtractor={item => item.id}
         />
         );
     }
