6ce189c21879b47d05fd52ec2f07d6fc2e2b6719
mariam <mariam2ac1@gmail.com>
2018-08-10 17:19:23 +0100

    Add duration field to event card component

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { formatDate } from '../api';
+import { formatDate, getCountdownParts } from '../api';
 import PropTypes from 'prop-types';
 
 export default function EventCard({ event }) {
+    const {
+        days,
+        hours,
+        minutes,
+        seconds,
+    } = getCountdownParts(event.date);
+
     return (
         <View>
             <View>
                 <Text>{formatDate(event.date)}</Text>
                 <Text>{event.title}</Text>
+            </View>
+            <View>
+                <Text>{days}</Text>
+                <Text>{hours}</Text>
+                <Text>{minutes}</Text>
+                <Text>{seconds}</Text>
             </View>
         </View>
     );
